fc6d799573a537451dbf87f670925a9b751fcf7d
Darius Foo <darius.foo.tw@gmail.com>
2022-03-08 11:12:12 +0800

    Interact functionality in stock toplevel

--- ppx/ppx_interact.ml
+++ ppx/ppx_interact.ml
@@ -1,5 +1,9 @@
 open Ppxlib
-module UTop_main = UTop_main
+module Ast = Ast_builder.Default
+
+(* module UTop_main = UTop_main *)
+(* module LNoise = LNoise *)
+(* module Interact = Interact *)
 
 type string = label
 
@@ -7,12 +11,14 @@ let get_name p =
   match p.ppat_desc with Ppat_var { txt = s; _ } -> [s] | _ -> []
 
 let rec build_list ~loc xs =
-  let open Ast_helper in
+  (* let open Ast_helper in *)
   match xs with
-  | [] -> Exp.construct ~loc { txt = Lident "[]"; loc } None
-  | x :: xs1 ->
-    Exp.construct ~loc { txt = Lident "::"; loc }
-      (Some (Exp.tuple ~loc [x; build_list ~loc xs1]))
+  | [] ->
+    (* Exp.construct ~loc { txt = Lident "[]"; loc } None *)
+    [%expr []]
+  | x :: xs1 -> [%expr [%e x] :: [%e build_list ~loc xs1]]
+(* Exp.construct ~loc { txt = Lident "::"; loc } *)
+(* (Some (Exp.tuple ~loc [x; build_list ~loc xs1])) *)
 
 let traverse () =
   object
@@ -21,39 +27,87 @@ let traverse () =
     method! value_binding vb env =
       let (v, _) = super#value_binding vb env in
       let name = get_name v.pvb_pat in
+      List.iter (fun n -> Format.printf "added: %s@." n) name;
       (v, name @ env)
 
     method! structure_item s env =
       let (s1, env) = super#structure_item s env in
-      (* TODO handle recursive bindings *)
+      (* TODO mutually recursive bindings *)
       match s.pstr_desc with Pstr_value (_, _) | _ -> (s1, env)
 
     method! expression e env =
       let open Ast_helper in
-      let (e, env) = super#expression e env in
+      (* TODO try moving this into the branches *)
+      (* it does help *)
+      (* let (e, env) = super#expression e env in *)
       match e.pexp_desc with
+      (* tried to generate a binding. but this didn't work *)
+      (* | Pexp_fun
+           ( label,
+             opt,
+             ({ ppat_desc = Ppat_var { txt = v; loc }; _ } as pat),
+             body ) ->
+         Format.printf "added: %s@." v;
+         (* let (e, env) = super#expression e env in *)
+         (* (e, v :: env) *)
+         (* recurse into body *)
+         let (body, env) = super#expression body (v :: env) in
+         let bind =
+           Ast.pexp_let ~loc Nonrecursive
+             [
+               Ast.value_binding ~loc
+                 ~pat:(Ast.ppat_var ~loc { loc; txt = v })
+                 ~expr:(Ast.pexp_ident ~loc { loc; txt = Lident v });
+             ]
+             body
+         in
+         (Ast.pexp_fun ~loc label opt pat bind, env) *)
       | Pexp_fun (_, _, { ppat_desc = Ppat_var { txt = v; _ }; _ }, _) ->
-        (e, v :: env)
+        Format.printf "added: %s@." v;
+        (* update, then recurse into subexpressions *)
+        let (e, env) = super#expression e (v :: env) in
+        (e, env)
       | Pexp_extension ({ txt = s; _ }, _payload) when String.equal s "interact"
         ->
+        List.iter (fun c -> Format.printf "ctx: %s@." c) env;
         let loc = e.pexp_loc in
         let elt e =
           let s = Exp.constant ~loc (Const.string ~loc e) in
           let id = Exp.ident ~loc { txt = Lident e; loc } in
           [%expr V ([%e s], [%e id])]
         in
-        let debug = Ast_builder.Default.estring ~loc (String.concat ", " env) in
-        ( [%expr
+        let debug = Ast.estring ~loc (String.concat ", " env) in
+        let status_print =
+          [%expr
             Format.printf
               "At line %d in module %s, with %d variables in scope.\n\n%s\n\n@."
               __LINE__ __MODULE__
               [%e Exp.constant ~loc (Const.int (List.length env))]
-              [%e debug];
-            Ppx_interact.UTop_main.interact ~unit:__MODULE__ ~loc:__POS__
-              ~values:[%e build_list ~loc (List.map elt env)]
-              ()],
+              [%e debug]]
+        in
+        let utop = false in
+        let linenoise = true in
+        let toplevel =
+          match (utop, linenoise) with
+          | (true, _) ->
+            [%expr
+              Ppx_interact.UTop_main.interact ~unit:__MODULE__ ~loc:__POS__
+                ~values:[%e build_list ~loc (List.map elt env)]
+                ()]
+          | (false, false) -> [%expr Topmain.main () |> ignore]
+          | (false, true) ->
+            [%expr
+              Ppx_interact_runtime.interact ~unit:__MODULE__ ~loc:__POS__
+                ~values:[%e build_list ~loc (List.map elt env)]
+                ()]
+        in
+        ( [%expr
+            [%e status_print];
+            [%e toplevel]],
           env )
-      | _ -> (e, env)
+      | _ ->
+        (* (e, env) *)
+        super#expression e env
   end
 
 let transform_impl str =
---
+++ runtime/ppx_interact_runtime.ml
@@ -0,0 +1,182 @@
+let eval text =
+  let lexbuf = Lexing.from_string text in
+  let phrase = !Toploop.parse_toplevel_phrase lexbuf in
+  ignore (Toploop.execute_phrase false Format.std_formatter phrase)
+
+let get_required_label name args =
+  match List.find (fun (lab, _) -> lab = Asttypes.Labelled name) args with
+  | (_, x) -> x
+  | exception Not_found -> None
+
+exception Found of Env.t
+exception Term of int
+
+type value = V : string * _ -> value
+
+(* let common_init ~initial_env =
+   (* Initializes toplevel environment. *)
+   (match initial_env with
+   | None -> Toploop.initialize_toplevel_env ()
+   | Some env ->
+     print_endline "set env";
+     Toploop.toplevel_env := env);
+   (* Set the global input name. *)
+   (* Make sure SIGINT is catched while executing OCaml code. *)
+   Sys.catch_break true;
+
+   (* Load system init files. *)
+
+   (* Load history after the initialization file so the user can change
+      the history file name. *)
+   (* Install signal handlers. *)
+   let behavior = Sys.Signal_handle (fun signo -> raise (Term signo)) in
+   let catch signo =
+     try Sys.set_signal signo behavior
+     with _ -> (* All signals may not be supported on some OS. *)
+               ()
+   in
+   (* We lost the terminal. *)
+   catch Sys.sighup;
+   (* Termination request. *)
+   catch Sys.sigterm *)
+
+let walk dir ~init ~f =
+  let rec loop dir acc =
+    let acc = f dir acc in
+    ArrayLabels.fold_left (Sys.readdir dir) ~init:acc ~f:(fun acc fn ->
+        let fn = Filename.concat dir fn in
+        match Unix.lstat fn with
+        | { st_kind = S_DIR; _ } -> loop fn acc
+        | _ -> acc)
+  in
+  match Unix.lstat dir with
+  | exception Unix.Unix_error (ENOENT, _, _) -> init
+  | _ -> loop dir init
+
+let interact ?(search_path = []) ?(build_dir = "_build") ~unit
+    ~loc:(fname, lnum, cnum, _) ~values () =
+  Toploop.initialize_toplevel_env ();
+  let search_path =
+    walk build_dir ~init:search_path ~f:(fun dir acc -> dir :: acc)
+  in
+  let cmt_fname =
+    try Misc.find_in_path_uncap search_path (unit ^ ".cmt")
+    with Not_found ->
+      Printf.ksprintf failwith "%s.cmt not found in search path!" unit
+  in
+  (* print_endline cmt_fname; *)
+  let cmt_infos = Cmt_format.read_cmt cmt_fname in
+  let expr next (e : Typedtree.expression) =
+    match e.exp_desc with
+    | Texp_apply (_, args) ->
+      begin
+        try
+          match
+            (get_required_label "loc" args, get_required_label "values" args)
+          with
+          | (Some l, Some v) ->
+            let pos = l.exp_loc.loc_start in
+            if
+              pos.pos_fname = fname && pos.pos_lnum = lnum
+              && pos.pos_cnum - pos.pos_bol = cnum
+            then
+              raise (Found v.exp_env)
+          | _ -> next e
+        with Not_found -> next e
+      end
+    | _ -> next e
+  in
+  let next iterator e = Tast_iterator.default_iterator.expr iterator e in
+  let expr iterator = expr (next iterator) in
+  let iter = { Tast_iterator.default_iterator with expr } in
+  let search = iter.structure iter in
+  try
+    begin
+      match cmt_infos.cmt_annots with
+      | Implementation st -> search st
+      | _ -> ()
+    end;
+    failwith "Couldn't find location in cmt file"
+  with Found env ->
+    (* print_endline "found loc"; *)
+    (try
+       List.iter Topdirs.dir_directory (search_path @ cmt_infos.cmt_loadpath);
+       let env = Envaux.env_of_only_summary env in
+       (* let _ = values in *)
+       List.iter
+         (fun (V (name, v)) -> Toploop.setvalue name (Obj.repr v))
+         values;
+       (* Toploop.setvalue "a" (Obj.repr a); *)
+       (* Toploop.toplevel_env := env; *)
+       Toploop.toplevel_env := env;
+       (* let idents = Env.diff Env.empty env in *)
+       (* List.iter print_endline (List.map Ident.name idents); *)
+       let eval text =
+         let lexbuf = Lexing.from_string text in
+         let phrase = !Toploop.parse_toplevel_phrase lexbuf in
+         ignore (Toploop.execute_phrase true Format.std_formatter phrase)
+       in
+
+       (* print_endline "about to eval"; *)
+
+       (* this worked *)
+       (* eval "b;;"; *)
+       (* eval "let c = b + 1;;"; *)
+       (* let v : int = Obj.obj (Toploop.getvalue "c") in *)
+       (* Format.printf "thing %d@." v; *)
+       (* print_endline "ok"; *)
+
+       (* Toploop.execute_phrase; *)
+       (* Toploop.execute_phrase true Format.std_formatter phrase; *)
+       (* Toploop.read_interactive_input := read_input_classic; *)
+       (* this doesn't work *)
+       (* Toploop.loop Format.std_formatter *)
+       (* while true do *)
+       (* let s = read_line () in *)
+       (* eval s *)
+       let rec user_input prompt cb =
+         match LNoise.linenoise prompt with
+         | None -> ()
+         | Some v ->
+           cb v;
+           user_input prompt cb
+       in
+       user_input "> " (fun s ->
+           if s = "quit" then exit 0;
+           (* LNoise.history_add from_user |> ignore; *)
+           (* LNoise.history_save ~filename:"history.txt" |> ignore; *)
+           (* Printf.sprintf "Got: %s" from_user |> print_endline *)
+           eval s)
+       (* done *)
+     with exn ->
+       Location.report_exception Format.err_formatter exn;
+       exit 2)
+(*
+let main () =
+  (* let ppf = Format.err_formatter in
+     (* let program = "ocaml" in *)
+     Compenv.readenv ppf Before_args;
+     (* Clflags.add_arguments __LOC__ Options.list; *)
+     (* Compenv.parse_arguments ~current argv file_argument program; *)
+     Compenv.readenv ppf Before_link;
+     Compmisc.read_clflags_from_env ();
+     (* if not (prepare ppf) then raise (Compenv.Exit_with_status 2); *)
+     Compmisc.init_path ();
+     eval "let a = 0;;";
+     Toploop.setvalue "a" (Obj.repr a);
+     Toploop.loop Format.std_formatter *)
+  (* interact () *)
+
+  (* Ppx_interact.UTop_main. *)
+  let b = 2 in
+  interact ~unit:__MODULE__ ~loc:__POS__
+    ~values:[V ("b", b); V ("walk", walk)]
+    ()
+(* ;
+
+   let d = Lib.info in
+   let b = 2 in
+   [%interact];
+   let c = 3 in
+   [%interact] *)
+*)
